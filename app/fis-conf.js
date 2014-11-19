ade025d723f5b3ee4721807925d24e1c5256f88b
chenhao <wy.chenhao@qq.com>
2014-11-19 11:36:12 +0800

    工程化配置

--- app/fis-conf.js
+++ app/fis-conf.js
@@ -1,19 +1,16 @@
 var now = new Date();
-fis.config.set('timestamp', [now.getFullYear(), now.getMonth() + 1, now.getDate(), now.getHours()].join(''));
+fis.config.set('timestamp', [now.getFullYear(), now.getMonth() + 1, now.getDate(), now.getHours(), now.getMinutes()].join(''));
 fis.config.set('modules.postpackager', 'seajs');
 fis.config.set('modules.spriter', 'csssprites');
 fis.config.merge({
     project:{
         // include : /(script|style|image|ajax)/ 设置源码文件的不知道怎么写
-        // exclude：/(script|style|image|ajax)/ 过滤源码文件的不知道怎么写
+        exclude:'iis.exe'
     },
     roadmap: {
-        domain: {
-            //所有css文件添加http://localhost:8080作为域名
-            '**.css': 'http://localhost:8080'
-        },
+        domain :'http://qian.wdaics.com/test/wx',
         path: [{
-            reg: /.*\.(js|css|jpg|png|gif|eot|svg|ttf|woff)$/,
+            reg: /.*\.(js|css|jpg|png|gif|eot|svg|ttf|woff|less)$/,
             query: '?t=${timestamp}',
             useSprite: true,
             useHash: false
@@ -45,22 +42,3 @@ fis.config.merge({
         }
     }
 });
-
-// //export, module, require不压缩变量名
-// fis.config.set('settings.optimizer.uglify-js', {
-//     mangle: {
-//         except: 'exports, module, require, define'
-//     }
-// });
-// //自动去除console.log等调试信息
-// fis.config.set('settings.optimizer.uglify-js', {
-//     compress: {
-//         drop_console: true
-//     }
-// });
-// // fis.config.set('settings.spriter.csssprites', {
-// //     //图之间的边距
-// //     margin: 10,
-// //     //使用矩阵排列方式，默认为线性`linear`
-// //     layout: 'matrix'
-// // });
